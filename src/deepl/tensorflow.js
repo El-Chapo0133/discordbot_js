fec58431dab06361799ff172dddacafa1c063f42
KingDotCom <levequeloris66@gmail.com>
2020-03-11 08:00:09 +0100

    restored src/deepl/* from commit f630a4a and 23cecc1

---
+++ src/deepl/tensorflow.js
@@ -0,0 +1,28 @@
+/**
+ * Author : Loris Levêque
+ * Date : 16.02.2020
+ * Description : ml file for the tensorflow-tfjs library
+ */
+
+const tensorflow = require('@tensorflow-tfjs');
+
+class Brain {
+	constructor() {
+		//require('@tensorflow-tfjs-node');
+		//require('@tensorflow-tfjs-node-gpu');
+	}
+	init(options) {
+
+	}
+	addLayer(layer) {
+
+	}
+	compile(options) {
+
+	}
+	predict(input) {
+
+	}
+}
+
+module.exports = new Brain();
